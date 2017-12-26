4711f28db8cabd870e9e316fbfeeea38a905eb0b
Maxim Petischak <petrischak.maxim@gmail.com>
2017-12-27 01:29:27 +0200

    Начал реализацию слайдера

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function($) {
+
+  /* Slick slider http://kenwheeler.github.io
+  ===========================================*/
+  $('#js-slides').slick({
+    dots: true,
+    infinite: true,
+    //speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false
+  })
+
+
+
+});
